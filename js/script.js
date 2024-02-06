3a60a003bae56cb630d9eab8d225a973e5416503
Nikita Korshak <128974328+ANP-Player@users.noreply.github.com>
2024-02-06 21:52:18 +0300

    2.0

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.carousel').slick({
+	autoplay: true,
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	prevArrow:"<img src = 'img/Arrow_left.svg'>",
+  nextArrow:"<img src = 'img/Arrow_right.svg'>",
+ 
+  autoplaySpeed: 1000,
+  arrows: true,
+  speed: 2000,
+  respondTo: 'window',
+ 
+   centerPadding: '1500px',
+  });
+ 
